f8ed40d8e3030c4d25479b3318e234314a505882
jerichocalibuso <jerichocalibuso@gmail.com>
2021-05-05 11:43:59 +0800

    feat: working backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,47 +6,113 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/create-todo", async (req, res) => {
+// app.post("/todo", async (req, res) => {
+//   const { task } = req.body;
+//   const result = await prisma.todo.create({
+//     data: {
+//       task,
+//     },
+//   });
+//   res.json(result);
+// });
+
+// app.get("/todos", async (req, res) => {
+//   const todos = await prisma.todo.findMany();
+//   res.json(todos);
+// });
+
+// app.put("/todo/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const { isDone } = await prisma.todo.findUnique({
+//     where: {
+//       id: String(id),
+//     },
+//   });
+//   const todo = await prisma.todo.update({
+//     where: {
+//       id: String(id),
+//     },
+//     data: {
+//       isDone: !isDone,
+//     },
+//   });
+//   res.json(todo);
+// });
+
+// app.delete("/todo/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const todo = await prisma.todo.delete({
+//     where: {
+//       id: String(id),
+//     },
+//   });
+//   res.json(todo);
+// });
+
+// Create project
+app.post("/projects", async (req, res) => {
+  const { title } = req.body;
+  try {
+    const project = await prisma.project.create({
+      data: {
+        title: title,
+      },
+    });
+    res.json(project);
+  } catch (e) {
+    console.error(`Error on creating a project, Error: ${e}`);
+  }
+});
+
+// Get projects and its todos
+app.get("/projects", async (req, res) => {
+  try {
+    const projects = await prisma.project.findMany({
+      include: {
+        todos: true,
+      },
+    });
+    res.json(projects);
+  } catch (e) {
+    console.error(`Error on updating a project todo, Error: ${e}`);
+  }
+});
+
+// Add project todo
+app.post("/projects/:id", async (req, res) => {
+  const { id: projectId } = req.params;
   const { task } = req.body;
-  const result = await prisma.todo.create({
-    data: {
-      task,
-    },
-  });
-  res.json(result);
-});
 
-app.get("/get-todos", async (req, res) => {
-  const todos = await prisma.todo.findMany();
-  res.json(todos);
-});
+  try {
+    const result = await prisma.todo.create({
+      data: {
+        task: task,
+        Project: { connect: { id: Number(projectId) } },
+      },
+    });
 
-app.put("/update-todo/:id", async (req, res) => {
-  const { id } = req.params;
-  const { isDone } = await prisma.todo.findUnique({
-    where: {
-      id: String(id),
-    },
-  });
-  const todo = await prisma.todo.update({
-    where: {
-      id: String(id),
-    },
-    data: {
-      isDone: !isDone,
-    },
-  });
-  res.json(todo);
+    res.json(result);
+  } catch (e) {
+    console.error(`Error on creating a project todo, Error: ${e}`);
+  }
 });
+// Update project todo
+app.put("/projects/:id", async (req, res) => {
+  const { id: taskId, isDone } = req.body;
 
-app.delete("/delete-todo/:id", async (req, res) => {
-  const { id } = req.params;
-  const todo = await prisma.todo.delete({
-    where: {
-      id: String(id),
-    },
-  });
-  res.json(todo);
+  try {
+    const result = await prisma.todo.update({
+      where: {
+        id: Number(taskId),
+      },
+      data: {
+        isDone: !isDone,
+      },
+    });
+    res.json(result);
+  } catch (e) {
+    console.error(`Error on updating a project todo, Error: ${e}`);
+  }
 });
 
 const server = app.listen(3001, () =>
